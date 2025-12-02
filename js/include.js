1efc207cc5a4d1c054b8b5d5623dd342f7d2007c
Tesya Indriani <tesyahilal@gmail.com>
2025-12-03 02:10:07 +0800

    Update include.js

--- js/include.js
+++ js/include.js
@@ -1,13 +1,29 @@
 // Load Navbar
-fetch("/components/navbar.html")
-  .then(res => res.text())
-  .then(data => {
-      document.getElementById("navbar-placeholder").innerHTML = data;
-  });
+// fetch("/components/navbar.html")
+//   .then(res => res.text())
+//   .then(data => {
+//       document.getElementById("navbar-placeholder").innerHTML = data;
+//   });
 
 // Load Footer
-fetch("/components/footer.html")
-  .then(res => res.text())
-  .then(data => {
-      document.getElementById("footer-placeholder").innerHTML = data;
-  });
+// fetch("/components/footer.html")
+//   .then(res => res.text())
+//   .then(data => {
+//       document.getElementById("footer-placeholder").innerHTML = data;
+//   });
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("navbar.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("navbar-placeholder").innerHTML = data;
+        });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("navbar-placeholder").innerHTML = data;
+        });
+});
